8680ab4bcda605567fad0d59a86bfa83bfcc044f
drinfernoo <drinfernoo@yahoo.com>
2019-03-12 11:01:12 -0700

    Add '.idea' to list of exclusions

--- _repo_xml_generator.py
+++ _repo_xml_generator.py
@@ -44,7 +44,7 @@ class Generator:
             zip = zipfile.ZipFile(final_zip, 'w', compression=zipfile.ZIP_DEFLATED )
             root_len = len(os.path.dirname(os.path.abspath(addon_id)))
             
-            ignore = ['.git', '.github', '.gitignore', '.DS_Store', 'thumbs.db']
+            ignore = ['.git', '.github', '.gitignore', '.DS_Store', 'thumbs.db', '.idea']
             
             for root, dirs, files in os.walk(addon_id):
                 # remove any unneeded git artifacts
