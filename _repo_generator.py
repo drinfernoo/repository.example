bed20244227709b78206fcfdb3e133446bb61492
drinfernoo <drinfernoo@yahoo.com>
2022-04-05 16:02:47 -0700

    :bookmark: - Update repo script to version 5

--- _repo_generator.py
+++ _repo_generator.py
@@ -12,7 +12,7 @@ import zipfile
 
 from xml.etree import ElementTree
 
-SCRIPT_VERSION = 4
+SCRIPT_VERSION = 5
 KODI_VERSIONS = ["krypton", "leia", "matrix", "nexus", "repo"]
 IGNORE = [
     ".git",
